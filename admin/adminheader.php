d7e2b4177f68a9622960be5e16ed18c22d159d5c
phongtran <phongtranhk20@gmail.com>
2024-03-12 22:25:32 +0700

    Add film, Sort adminPage

--- admin/adminheader.php
+++ admin/adminheader.php
@@ -39,7 +39,7 @@
             </li>
 
             <li class="">
-                <a href="#"><i class="material-icons">date_range</i><span>Thêm phim</span></a>
+                <a href="./addfilm.php"><i class="material-icons">date_range</i><span>Thêm phim</span></a>
             </li>
 
             <li class="">
---
+++ inc/footer.php
@@ -0,0 +1,83 @@
+<!-- Footer -->
+<footer class="footer">
+  <div class="main-content">
+    <div class="row">
+      <!-- Column 1 -->
+      <div class="column">
+        <img src="./uploads/img/phimmoi.png" alt="Phimmoi" class="logo" />
+        <p class="desc">
+          <a href="#!">Phimmoi</a> - Trang xem phim Online với giao diện
+          mới được bố trí và thiết kế thân thiện với người dùng. Nguồn
+          phim được tổng hợp từ các website lớn với đa dạng các đầu phim
+          và thể loại vô cùng phong phú
+        </p>
+      </div>
+      <!-- Column 2 -->
+      <div class="column">
+        <h3 class="title">Phim mới</h3>
+        <ul class="list">
+          <li>
+            <a href="#!">Phim Khoa Học</a>
+          </li>
+          <li>
+            <a href="#!">Phim Kinh Dị</a>
+          </li>
+          <li>
+            <a href="#!">Phim Chiếu Rạp</a>
+          </li>
+          <li>
+            <a href="#!">Phim Hình Sự</a>
+          </li>
+          <li>
+            <a href="#!">Phim Hành Động</a>
+          </li>
+        </ul>
+      </div>
+      <!-- Column 3 -->
+      <div class="column">
+        <h3 class="title">Phim hay</h3>
+        <ul class="list">
+          <li>
+            <a href="#!">Phim Âu Mỹ</a>
+          </li>
+          <li>
+            <a href="#!">Phim Hàn Quốc</a>
+          </li>
+          <li>
+            <a href="#!">Phim Trung Quốc</a>
+          </li>
+          <li>
+            <a href="#!">Phim Nhật Bản</a>
+          </li>
+          <li>
+            <a href="#!">Phim Thái Lan</a>
+          </li>
+        </ul>
+      </div>
+      <!-- Column 4 -->
+      <div class="column">
+        <h3 class="title">Thông tin</h3>
+        <ul class="list">
+          <li>
+            <a href="#!">Giới thiệu</a>
+          </li>
+          <li>
+            <a href="#!">Liên hệ chúng tôi</a>
+          </li>
+          <li>
+            <a href="#!">Điều khoản sử dụng</a>
+          </li>
+          <li>
+            <a href="#!">Chính sách riêng tư</a>
+          </li>
+          <li>
+            <a href="#!">Khiếu nại bản quyền</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="copyright">
+      <p>Copyright ©2022 webdesign.gdn All rights reserved</p>
+    </div>
+  </div>
+</footer>
